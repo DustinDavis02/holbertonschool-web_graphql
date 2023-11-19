43ca0935e74ca6abc9ba660dd590874af789725c
Dustin Davis <davisdustin328@gmail.com>
2023-11-19 17:18:19 -0600

    test simple server

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema.js');
+// const express = require('express');
+// const { graphqlHTTP } = require('express-graphql');
+// const schema = require('./schema/schema.js');
 
+// const app = express();
+
+// app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   graphiql: true
+// }));
+
+// app.listen(4000, () => {
+//   console.log('now listening for requests on port 4000');
+// });
+
+const express = require('express');
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true
-}));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-app.listen(5000, () => {
-  console.log('now listening for requests on port 4000');
+app.listen(4000, () => {
+  console.log('Server running on port 4000');
 });
