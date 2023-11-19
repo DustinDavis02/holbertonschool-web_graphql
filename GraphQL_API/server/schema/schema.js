c3b480f2062c4e24f0d19b7adf5c83d5cbb0af0c
Dustin Davis <davisdustin328@gmail.com>
2023-11-19 17:05:32 -0600

    Root Query

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,5 +1,6 @@
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
 
+// TaskType definition
 const TaskType = new GraphQLObjectType({
     name: 'Task',
     fields: {
@@ -10,4 +11,18 @@ const TaskType = new GraphQLObjectType({
     }
 });
 
-module.exports = TaskType;
+// RootQuery definition
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+      task: {
+          type: TaskType,
+          args: { id: { type: GraphQLString } },
+          // Resolve function
+          resolve(parent, args) {
+          }
+      }
+  }
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery });
