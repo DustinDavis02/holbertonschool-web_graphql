62179f8afa9f174cd735f686dc73b20a4a3197a5
Dustin Davis <davisdustin328@gmail.com>
2023-11-19 17:22:42 -0600

    revert changes

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,25 +1,14 @@
-// const express = require('express');
-// const { graphqlHTTP } = require('express-graphql');
-// const schema = require('./schema/schema.js');
-
-// const app = express();
-
-// app.use('/graphql', graphqlHTTP({
-//   schema: schema,
-//   graphiql: true
-// }));
-
-// app.listen(4000, () => {
-//   console.log('now listening for requests on port 4000');
-// });
-
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema.js');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true
+}));
 
 app.listen(4000, () => {
-  console.log('Server running on port 4000');
+  console.log('now listening for requests on port 4000');
 });
