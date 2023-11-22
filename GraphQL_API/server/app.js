4a9fc7165bae37f5ef468d6fa9e0308a0783fc35
Dustin Davis <davisdustin328@gmail.com>
2023-11-22 17:33:11 -0600

    update App.js to allow connection to mongoose

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,6 +9,11 @@ app.use('/graphql',graphqlHTTP({
   schema,
   graphiql: true,
 }));
+
+mongoose.connect('mongodb+srv://davisdustin501:<Test123>@cluster0.d4hthsw.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to database');
+});
 
 app.listen(4000,()=>{
   console.log('now listening for request on port 4000');
