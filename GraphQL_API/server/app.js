5f164d6b22a4dfd69b116a1fe5c0086bcf2dba76
Dustin Davis <davisdustin328@gmail.com>
2023-11-22 21:04:35 -0600

    syntax error

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -13,7 +13,7 @@ app.use('/graphql',graphqlHTTP({
   graphiql: true,
 }));
 
-mongoose.connect('mongodb+srv://davisdustin501:<Test123>@cluster0.d4hthsw.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://davisdustin501:Test123@cluster0.d4hthsw.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => {
   console.log('Connected to database');
 });
